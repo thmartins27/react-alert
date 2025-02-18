bfca1f7437fe85b2661851badf8d9dff4a20089e
thmartins27 <thiago.olivmartins@gmail.com>
2025-02-18 17:05:09 -0300

    tweak vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-})
+  build: {
+    outDir: 'dist', // Diretório de saída
+    lib: {
+      entry: './src/index.tsx', // Ponto de entrada do seu pacote
+      name: 'Alert', // Nome global da biblioteca (se usado em um ambiente sem módulos)
+      fileName: 'index', // Nome do arquivo de saída
+      formats: ['es', 'cjs'], // Formatos de módulo (ESM e CommonJS)
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'], // Dependências externas
+      output: {
+        globals: {
+          react: 'React', // Mapeia 'react' para a variável global 'React'
+          'react-dom': 'ReactDOM', // Mapeia 'react-dom' para a variável global 'ReactDOM'
+        },
+      },
+    },
+  },
+});
