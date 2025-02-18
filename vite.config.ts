b9dad1ea9bc32afa811a21e6907ec035248d5119
thmartins27 <thiago.olivmartins@gmail.com>
2025-02-18 17:12:14 -0300

    tweak vite.config.ts and tsconfig.app.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import dts from 'vite-plugin-dts'; // Plugin para gerar arquivos .d.ts
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    dts({ // Configuração do plugin para gerar arquivos .d.ts
+      insertTypesEntry: true, // Adiciona entradas de tipos no package.json
+      include: ['src'], // Inclui todos os arquivos da pasta src
+      outDir: 'dist/types', // Diretório de saída para os arquivos .d.ts
+    }),
+  ],
   build: {
     outDir: 'dist', // Diretório de saída
     lib: {
       entry: './src/index.tsx', // Ponto de entrada do seu pacote
-      name: 'Alert', // Nome global da biblioteca (se usado em um ambiente sem módulos)
+      name: 'Alert', // Nome global da biblioteca
       fileName: 'index', // Nome do arquivo de saída
       formats: ['es', 'cjs'], // Formatos de módulo (ESM e CommonJS)
     },
